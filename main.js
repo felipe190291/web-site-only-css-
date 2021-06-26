e8c45a2eea3b8f3a4dd9d38532a3d7d67bf5c64e
felipe190291 <pipe_cesped@hotmail.com>
2021-06-25 20:35:51 -0500

    level up

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ ScrollReveal().reveal(".showcase", { delay: 500 });
 ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
 ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
 ScrollReveal().reveal(".news-cards", { delay: 500 });
+document.querySelector("#switch").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  document.querySelector("#switch").classList.toggle("active");
+});
